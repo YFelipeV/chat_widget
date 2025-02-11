cafb525ff3fea36aecc66414a81eb3016f4193bb
YFelipeV <felipesegura250@gmail.com>
2025-02-11 11:29:36 -0500

    g01

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,34 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
+import inject from "@rollup/plugin-inject";
 
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
   define: {
     "process.env": {
-      NODE_ENV: "production", 
+      NODE_ENV: '"production"',
     },
   },
   build: {
     lib: {
-      entry: "src/ChatWidget.jsx", // Archivo principal de tu componente
+      entry: "src/ChatWidget.jsx",
       name: "ChatWidget",
       fileName: (format) => `chat-widget.${format}.js`,
     },
     rollupOptions: {
-      external: ["react", "react-dom"], // No incluir React en el bundle
+      external: ["react", "react-dom"],
       output: {
         globals: {
           react: "React",
           "react-dom": "ReactDOM",
         },
       },
+      plugins: [
+        inject({
+          process: "process",
+        }),
+      ],
     },
   },
 });
