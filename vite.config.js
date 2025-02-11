5fccc5e1df3e2fb3199ba0c6ad7e55f02269096c
YFelipeV <felipesegura250@gmail.com>
2025-02-11 16:03:44 -0500

    fill

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  mode:"production",
+  define:{
+    'process':{
+      env:{
+        NODE_ENV:JSON.stringify('production')
+      }
+    }
+  },
   build: {
     lib: {
       entry: resolve(__dirname, "src/main.jsx"),
@@ -22,10 +30,11 @@ export default defineConfig({
           "react/jsx-runtime": "react/jsx-runtime"
         }
       },
+      intro: 'const process = { env: { NODE_ENV: "production" } };'
+
       
     },
-    sourcemap: true,
-    minify:false,
+
     
   }
 });
