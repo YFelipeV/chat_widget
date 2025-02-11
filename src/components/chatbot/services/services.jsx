6011b9a9375e5421152cb065f87a4349f683ccef
YFelipeV <felipesegura250@gmail.com>
2025-02-11 12:06:54 -0500

    gg2

--- src/components/chatbot/services/services.jsx
+++ src/components/chatbot/services/services.jsx
@@ -1,8 +1,8 @@
 import {reqtsApiForm} from '../../../config/Utils'
 export const sendMessageApi = async (data) => {
-    return reqtsApiForm('v1/chatboot/sendmesaje', true, 'POST', data, data => data, error => error);
+    return reqtsApiForm('v1/chatboot/sendmesaje', 'POST', false);
 }
 
 export const getChatHistory= async () => {
-    return reqtsApiForm('v1/chatboot/history', true, 'POST', {}, data => data, error => error);
+    return reqtsApiForm('v1/chatboot/history', 'POST', false);
 }
