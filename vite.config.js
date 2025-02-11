afdcf43af4ceade569836b7fcd81af071c6e5e13
YFelipeV <felipesegura250@gmail.com>
2025-02-11 11:24:16 -0500

    v04

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import {nodePolyfills} from 'vite-plugin-node-polyfills'
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
-  plugins: [react(),nodePolyfills()],
-  define:{
-    "process.env": {},
+  plugins: [react(), nodePolyfills()],
+  define: {
+    "process.env": {
+      NODE_ENV: '"production"', // Definir un valor para NODE_ENV
+    },
   },
   build: {
     lib: {
